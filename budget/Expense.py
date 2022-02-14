fe3455a6d5f0e45ffa1ee952765bb7b74140091a
portisg3 <gretaportis@northwesternmutual.com>
2022-02-14 09:18:32 -0600

    module5

--- budget/Expense.py
+++ budget/Expense.py
@@ -46,4 +46,3 @@ class Expenses():
         food_expenses = {x for x in self.list if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return [necessary_expenses, food_expenses, unnecessary_expenses]
-        print(timeit.timeit(stmt = 'expenses.categorize_set_comprehension()', setup = 'from.import Expense\\expenses = Expense.Expenses()\\expenses.read_expenses("data/spending_data.csv")', number = 100000, globals=globals()))
